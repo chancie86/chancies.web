570ff1ad0c1beb1f1a51687ae1e3a1b0c2376f6d
Hoi Chan <22822185+chancie86@users.noreply.github.com>
2021-09-27 16:33:31 +0100

    Add health controller

---
+++ server/Api/Controllers/Health/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using chancies.Api.Controllers.Document.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chancies.Api.Controllers.Health
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController
+        : ControllerBase
+    {
+        public HealthController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<DocumentListItemDto>>> List()
+        {
+            return Ok();
+        }
+    }
+}
